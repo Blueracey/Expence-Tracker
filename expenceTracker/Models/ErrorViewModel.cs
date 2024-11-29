80094fc9fe39c57202f935a3d2ce8b4e813ca6b1
Corbyn <144940013+Blueracey@users.noreply.github.com>
2024-11-28 17:08:42 -0700

    Add project files.

---
+++ expenceTracker/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace expenceTracker.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
