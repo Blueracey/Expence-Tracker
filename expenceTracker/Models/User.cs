c8c4d8062afc94acd24003062f78088f0418ccc3
Corbyn <144940013+Blueracey@users.noreply.github.com>
2024-11-28 17:20:56 -0700

    Added user and user profile models
    with a functional forign key

--- expenceTracker/Models/User.cs
+++ expenceTracker/Models/User.cs
@@ -2,5 +2,8 @@
 {
     public class User
     {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
     }
 }
--- expenceTracker/Models/userProfile.cs
+++ expenceTracker/Models/userProfile.cs
@@ -1,6 +1,15 @@
-﻿namespace expenceTracker.Models
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace expenceTracker.Models
 {
     public class userProfile
     {
+        public int Id { get; set; }
+        public bool Darkmode { get; set; }
+        public int userId { get; set; }
+
+        [ForeignKey("userId")]
+        public User User { get; set; }
+        
     }
 }
