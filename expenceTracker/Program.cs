450e1fd0252d5ebf3fc6e1d768ed4d46f17b6fea
Corbyn <144940013+Blueracey@users.noreply.github.com>
2024-12-04 21:39:05 -0700

    login now works and creates a tokin on the browser
    I made the lowest effort home page ever produced

--- expenceTracker/Program.cs
+++ expenceTracker/Program.cs
@@ -58,6 +58,6 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=account}/{action=Index}/{id?}");
 
 app.Run();
